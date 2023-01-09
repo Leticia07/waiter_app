17d9ee44c1129fd8b8711bd43fa3e6f9d61cbabf
Letícia Aragão <leticia.aragao2208@gmail.com>
2023-01-09 09:38:33 -0300

    first commir

---
+++ src/Main/index.tsx
@@ -0,0 +1,29 @@
+import { Button } from '../components/Button';
+import { Categories } from '../components/Categories';
+import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer } from './styles';
+
+
+export function Main() {
+  return (
+    <>
+      <Container>
+        <Header/>
+        <CategoriesContainer>
+          <Categories/>
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu/>
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer>
+          <Button onPress={() => alert('Novo Pedido')}>
+              Novo Pedido
+          </Button>
+        </FooterContainer>
+      </Footer>
+    </>
+  );
+}
